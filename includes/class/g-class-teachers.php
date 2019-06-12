6b421ae18dee2e2dcc5d7d90249b172c7a0bd36d
gurcharan <mailtogagan990@gmail.com>
2019-06-12 22:22:19 +0530

    save local changes

--- includes/class/g-class-teachers.php
+++ includes/class/g-class-teachers.php
@@ -44,14 +44,15 @@ class Teachers_List extends WP_List_Table {
 	public static function create_teacher($data) {
 		global $wpdb;
 
-		$table = $wpdb->prefix."gs_teacher";
+		$table = $wpdb->prefix."gs_teacher";	
+
 		try {
 			$result = $wpdb->insert(  $table, $data );
-		} catch (\Throwable $th) {
+
+		} catch (\Exception $th) {
 			_e( 'Error occured', 'sp' );
 			print_r($th); die;
 		}
-		$wpdb->print_error(); die;
 		return $result;
 	}
 
